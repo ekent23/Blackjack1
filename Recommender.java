13e29ce98d54902ad8521c0b0697bf0b47fd3694
mrasamny <mrasamny@desu.edu>
2024-04-09 15:54:04 -0400

    Added recommender class

---
+++ Recommender.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Recommender here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Recommender extends Actor
+{
+    /**
+     * Act - do whatever the Recommender wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
